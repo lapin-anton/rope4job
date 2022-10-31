a1c0e8265b3cedebfd6921c7fd12f3b2434ebdbb
anton-lapin <lapin_antoine@mail.ru>
2022-10-31 12:09:32 +0300

    initial

---
+++ src/test/java/ru/lapinlisss/rope4job/Rope4jobApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lapinlisss.rope4job;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rope4jobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
